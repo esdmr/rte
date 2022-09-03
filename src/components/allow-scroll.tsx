eeeb020bbd6484d378a56b1a83c62a2e4d6eb221
esdmr <esdmr0@gmail.com>
2022-09-03 12:05:48 +0430

    Add missing assert in allow-scroll

--- src/components/allow-scroll.tsx
+++ src/components/allow-scroll.tsx
@@ -1,10 +1,12 @@
 import type {FunctionComponent} from 'preact';
 import {useEffect} from 'preact/hooks';
+import assert from '../assert.js';
 
 let set = false;
 
 export const AllowScroll: FunctionComponent = () => {
 	useEffect(() => {
+		assert(!set);
 		set = true;
 		document.body.classList.add('scroll');
 
