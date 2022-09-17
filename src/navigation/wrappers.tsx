e63c15d8df6815d1ab50a2fb0a1208cc29446916
esdmr <esdmr0@gmail.com>
2022-09-17 16:29:54 +0430

    Remove unneeded wrappers

--- src/navigation/wrappers.tsx
+++ src/navigation/wrappers.tsx
@@ -20,11 +20,7 @@ function createWrapper<T extends keyof JSX.IntrinsicElements>(name: T) {
 }
 
 export const A = /* @__PURE__ */ createWrapper('a');
-export const Area = /* @__PURE__ */ createWrapper('area');
 export const Input = /* @__PURE__ */ createWrapper('input');
 export const Select = /* @__PURE__ */ createWrapper('select');
 export const Textarea = /* @__PURE__ */ createWrapper('textarea');
 export const Button = /* @__PURE__ */ createWrapper('button');
-export const Iframe = /* @__PURE__ */ createWrapper('iframe');
-export const Object = /* @__PURE__ */ createWrapper('object');
-export const Embed = /* @__PURE__ */ createWrapper('embed');
